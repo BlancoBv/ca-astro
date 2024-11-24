0e6718d279eb1446fca901630fd1e25890f264f5
blanco <dehara14@gmail.com>
2024-11-24 12:52:13 -0600

    error de cors

--- src/assets/Axios.ts
+++ src/assets/Axios.ts
@@ -6,6 +6,7 @@ const PROD = import.meta.env.PROD;
 export default axios.create({
   baseURL: `${PROD ? import.meta.env.SITE : "http://localhost:4321"}/api/`,
   headers: {
+    "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   },
   /* validateStatus: (status: number) => {
