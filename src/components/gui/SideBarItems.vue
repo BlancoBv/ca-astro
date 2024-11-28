a58caf27b24ceb6beba990b7f20ab94cd79e9343
blanco <dehara14@gmail.com>
2024-11-27 19:29:10 -0600

    cambios en la sidebar

--- src/components/gui/SideBarItems.vue
+++ src/components/gui/SideBarItems.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { useGetData } from '@assets/http';
+import MiembrosSide from './MiembrosSide.vue';
+import ColaboradoresSide from './ColaboradoresSide.vue';
 
 const { data, isPending, isError } = useGetData("menus", "menus-nav")
 </script>
 <template>
 
     <li v-if="!isPending && !isError" v-for="menu in data.response">
-        <details v-if="menu.dropcollapse" open>
+        <template v-if="menu.ruta === 'miembros'">
+            <MiembrosSide />
+        </template>
+        <template v-else-if="menu.ruta === 'colaboradores'">
+            <ColaboradoresSide />
+        </template>
+
+        <details v-else-if="menu.dropcollapse" open>
             <summary>{{ menu.nombre }}</summary>
             <ul>
                 <li v-for="submenu in menu.submenus">
