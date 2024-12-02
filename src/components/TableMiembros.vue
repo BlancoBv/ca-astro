018ffb6fc21d1dc0141c70594a084c48d80a75c1
blanco <dehara14@gmail.com>
2024-12-02 15:10:33 -0600

    datatable de primevue para vista de mimbros

--- src/components/TableMiembros.vue
+++ src/components/TableMiembros.vue
@@ -7,7 +7,7 @@ import get from "lodash/get"
 
 
 interface props {
-    cols: { field: string, header: string, sortable?: boolean, formatter?: "uppercase" }[], data: any[]
+    cols: { field: string, header: string, sortable?: boolean, formatter?: "uppercase" | "colabs" }[], data: any[]
 }
 
 
@@ -15,7 +15,47 @@ interface props {
 const props = defineProps<props>()
 const isMounted = ref<boolean>(false)
 
-const formatter = { uppercase: (value: string) => value.toUpperCase() }
+
+const getCollabs = (element: {
+    otrosColaboradores: string | null;
+    miembros_proyecto: {
+        nombreCompleto: string;
+        nombre: string;
+        apepat: string;
+        apemat: string;
+    }[];
+}): string => {
+    const formatter = new Intl.ListFormat("es", {
+        type: "conjunction",
+        style: "long",
+    });
+    const miembrosF: string[] = [];
+    console.log(element);
+
+    element.miembros_proyecto.forEach((el) => {
+        miembrosF.push(el.nombreCompleto);
+    });
+
+    if (element.otrosColaboradores) {
+        const otherCollabsArr: string[] = element.otrosColaboradores.split(",");
+        miembrosF.push(...otherCollabsArr);
+    }
+
+    if (miembrosF.length < 1) {
+        return "----";
+    }
+
+    return formatter.format(miembrosF);
+};
+
+const formatter = { uppercase: (value: string) => value.toUpperCase(), colabs: (value: any) => getCollabs(value) }
+
+
+const handleClick = () => {
+
+    console.log("Row")
+
+}
 
 onMounted(() => {
     isMounted.value = true
@@ -29,19 +69,35 @@ console.log(props.cols);
 
 </script>
 <template>
-
-    <DataTable :value="props.data" paginator unstyled :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
-        pt:root="not-prose" pt:thead="bg-neutral text-neutral-content" pt:table="table table-zebra" pt:first="btn"
-        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-        currentPageReportTemplate="{first} to {last} of {totalRecords}">
+    <DataTable :value="props.data" :paginator="true" :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]"
+        selectionMode="single" @row-click="handleClick">
 
         <Column v-for="col of props.cols" :key="col.field" :field="col.field" :header="col.header"
             :sortable="col.sortable">
             <template #body="row">
-                {{ col.formatter ? formatter[col.formatter]?.(get(row.data, col.field)) : get(row.data, col.field) }}
+                {{ col.formatter ? formatter[col.formatter]?.(get(row.data, col.field, row.data)) : get(row.data,
+                    col.field)
+                }}
             </template>
         </Column>
+        <template #empty>
+            Sin datos.
+        </template>
 
+
+        <template
+            #paginatorcontainer="{ first, last, page, pageCount, prevPageCallback, nextPageCallback, totalRecords }">
+            <div
+                class="flex items-center gap-4 border border-primary bg-transparent rounded-full w-full py-1 px-2 justify-between">
+                <Button icon="pi pi-chevron-left" rounded text @click="prevPageCallback" :disabled="page === 0" />
+                <div class="text-color font-medium">
+                    <span class="hidden sm:block">Showing {{ first }} to {{ last }} of {{ totalRecords }}</span>
+                    <span class="block sm:hidden">Page {{ page + 1 }} of {{ pageCount }}</span>
+                </div>
+                <Button icon="pi pi-chevron-right" rounded text @click="nextPageCallback"
+                    :disabled="page === pageCount - 1" />
+            </div>
+        </template>
     </DataTable>
     <!-- <div class="skeleton h-96 w-full" v-else>
     </div> -->
--- src/pages/_app.ts
+++ src/pages/_app.ts
@@ -2,8 +2,6 @@ import type { App } from "vue";
 import { VueQueryPlugin } from "@tanstack/vue-query";
 import Vue3Toastify, { toast, type ToastContainerOptions } from "vue3-toastify";
 import PrimeVue from "primevue/config";
-import Aura from "@primevue/themes/aura";
-
 export default (app: App) => {
   app.use(VueQueryPlugin);
   app.use(Vue3Toastify, {
@@ -11,8 +9,30 @@ export default (app: App) => {
     position: toast.POSITION.TOP_RIGHT,
   } as ToastContainerOptions);
   app.use(PrimeVue, {
+    unstyled: true,
+    pt: {
+      datatable: {
+        root: "not-prose",
+        table: "table table-zebra",
+      },
+      paginator: {
+        root: "py-4",
+        content: "flex justify-center items-center gap-4 flex-wrap",
+        pages: "flex gap-4",
+        first: "btn btn-sm",
+        prev: "btn btn-sm",
+        next: "btn btn-sm",
+        last: "btn btn-sm",
+        page: "btn btn-sm data-[p-active=true]:btn-secondary",
+      },
+      select: {
+        root: "input input-bordered flex gap-2 items-center justify-center",
+        list: "menu bg-base-100 rounded-btn",
+        label: "outline-none",
+      },
+    },
     theme: {
-      preset: Aura,
+      /*       preset: Aura, */
       options: {
         darkModeSelector: false,
       },
