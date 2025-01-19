efe55eea320f2d91db995eb2cb4f7931a1144b10
blanco <dehara14@gmail.com>
2025-01-19 09:01:23 -0600

    cambios en carousel

--- src/components/gui/Carousel.vue
+++ src/components/gui/Carousel.vue
@@ -1,29 +1,39 @@
 <script setup lang="ts">
-import { useGetData } from "@assets/http";
-import { Carousel, Navigation, Slide } from "vue3-carousel"
+import { onMounted, onUnmounted, ref } from "vue";
+import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel"
 import 'vue3-carousel/dist/carousel.css'
 
-const { data, isPending, isError } = useGetData("banners?mostrar=true", "bannersData") 
+const props = defineProps<{ banners: { createdAt: string, url: string, descripcion: string, idbanner: string, imagen: string }[] }>()
+
+const isMounted = ref<boolean>(false)
+
+onMounted(() => {
+    isMounted.value = true
+
+})
+onUnmounted(() => {
+    isMounted.value = false
+})
+
+
 </script>
 <template>
-    <Carousel v-if="!isPending && !isError" class="block" wrap-around :breakpoints="{
+    <Carousel class="block" :class="{ 'cursor-wait select-none': !isMounted }" wrap-around :breakpoints="{
         300: {
             itemsToShow: 1.3,
             snapAlign: 'start',
         },
-
         // 500px and up
         640: {
             itemsToShow: 1.5,
             snapAlign: 'center',
 
-        },
-
+        }
     }">
 
         <template #slides>
-            <Slide class="h-60 md:h-96 w-full" v-for="banner in data.response" :key="banner.idbanner">
-                <div class="h-full w-full relative flex items-end justify-end">
+            <Slide class="w-full h-60 md:h-96 select-none" v-for="banner in props.banners" :key="banner.idbanner">
+                <div class="h-full w-full relative flex items-end justify-end" :class="{ 'skeleton': !isMounted }">
                     <img class="w-full h-full object-cover -z-10 absolute top-0 left-0 rounded-box" :src="banner.imagen"
                         width="50px" height="50px" :alt="banner.createdAt" loading="eager">
 
@@ -33,25 +43,24 @@ const { data, isPending, isError } = useGetData("banners?mostrar=true", "banners
                             <div class="card-body">
                                 <p v-if="banner.descripcion">{{ banner.descripcion }}</p>
                                 <div class="card-actions justify-end" v-if="banner.url">
-                                    <a target="_blank" :href="banner.url" class="btn btn-primary btn-sm">Ver enlace
-                                        adjunto</a>
+                                    <a target="_blank" :href="banner.url" class="btn btn-primary btn-sm">
+                                        Ver enlace adjunto
+                                    </a>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
             </Slide>
         </template>
-
-
         <template #addons>
             <Navigation />
+            <Pagination />
         </template>
     </Carousel>
-    <div v-else class="h-60 md:h-[384px] w-full flex flex-col gap-4">
+    <!-- <div v-else class="h-60 md:h-[384px] w-full flex flex-col gap-4">
         <div class="skeleton w-full grow"></div>
-    </div>
+    </div> -->
 </template>
 <style>
 .carousel__slide {
@@ -100,9 +109,16 @@ const { data, isPending, isError } = useGetData("banners?mostrar=true", "banners
     @apply w-12 md:w-20;
 }
 
-
-
 .carousel__icon {
     font-size: 72pt;
+}
+
+.carousel__pagination-button {
+    @apply h-4 w-8;
+    font-size: 72pt;
+}
+
+.carousel__pagination-button::after {
+    @apply w-full h-full
 }
 </style>
