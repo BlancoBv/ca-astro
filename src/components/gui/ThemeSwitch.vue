d2b66d69f7fe059c9169ba541f283397371569a6
BlancoBv <dehara14@gmail.com>
2025-09-14 22:15:54 -0600

    cambio en clases css y cambiador de modo oscuro

--- src/components/gui/ThemeSwitch.vue
+++ src/components/gui/ThemeSwitch.vue
@@ -1,36 +1,40 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 
-const active = ref<boolean>(false)
+const active = ref<boolean>(false);
 
 const changeTheme = (checked: boolean) => {
-    const theme = checked ? "night" : "winter"
-    document.documentElement.setAttribute("data-theme", theme)
-}
+  const theme = checked ? "dark" : "winter";
+  document.documentElement.setAttribute("data-theme", theme);
+};
 const handle = (ev: Event) => {
-    const { checked } = ev.target as HTMLInputElement
-    active.value = checked
-    localStorage.setItem("isDark", JSON.stringify(checked))
-    changeTheme(checked)
-
-}
+  const { checked } = ev.target as HTMLInputElement;
+  active.value = checked;
+  localStorage.setItem("isDark", JSON.stringify(checked));
+  changeTheme(checked);
+};
 onMounted(() => {
-    const isDark: boolean = JSON.parse(localStorage.getItem("isDark") ?? "false")
-    active.value = isDark
-})
+  const isDark: boolean = JSON.parse(localStorage.getItem("isDark") ?? "false");
+  active.value = isDark;
+});
 </script>
 <template>
-    <label class="swap swap-rotate">
-        <!-- this hidden checkbox controls the state -->
-        <input type="checkbox" class="theme-controller" @input="handle" :checked="active" />
+  <label class="swap swap-rotate">
+    <!-- this hidden checkbox controls the state -->
+    <input
+      type="checkbox"
+      class="theme-controller"
+      @input="handle"
+      :checked="active"
+    />
 
-        <!-- sun icon -->
-        <div class="swap-on fill-current flex justify-center items-center size-10">
-            <i class="bi bi-sun-fill text-xl"></i>
-        </div>
-        <div class="swap-off fill-current flex justify-center items-center size-10">
-            <i class="bi bi-moon-fill text-xl"></i>
-        </div>
-        <!-- moon icon -->
-    </label>
+    <!-- sun icon -->
+    <div class="swap-on fill-current flex justify-center items-center size-10">
+      <i class="bi bi-sun-fill text-xl"></i>
+    </div>
+    <div class="swap-off fill-current flex justify-center items-center size-10">
+      <i class="bi bi-moon-fill text-xl"></i>
+    </div>
+    <!-- moon icon -->
+  </label>
 </template>
